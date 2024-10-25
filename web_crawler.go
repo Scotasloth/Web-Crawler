f8e8e96c3972e4c2518fcb2b9e435a0978bb99d9
Scotasloth <rossmcc76@gmail.com>
2024-10-25 20:53:24 +0100

    Finished first draft of code

--- web_crawler.go
+++ web_crawler.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
+
+	"golang.org/x/net/html"
 )
 
 func getLinks(url string) ([]string, error) {
@@ -18,7 +22,33 @@ func getLinks(url string) ([]string, error) {
 		return nil, fmt.Errorf("Failed to get URL: %s", url)
 	}
 
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	doc, err := html.Parse(strings.NewReader(string(body)))
+	if err != nil {
+		return nil, err
+	}
+
 	var links []string
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, attr := range n.Attr {
+				if attr.Key == "href" {
+					links = append(links, attr.Val)
+				}
+			}
+		}
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+
+	f(doc)
 	return links, nil
 }
 
