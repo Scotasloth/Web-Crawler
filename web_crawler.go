123d348e37e2e441b4a9016caff068c2aad08df3
Scotasloth <rossmcc76@gmail.com>
2024-10-25 19:56:59 +0100

    Added some basic code

--- web_crawler.go
+++ web_crawler.go
@@ -1,9 +1,30 @@
 package main
 
-func getLinks() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func getLinks(url string) ([]string, error) {
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	return links, nil
 }
 
 func main() {
+	url := "Testsite"
 
+	links, err := getLinks(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("Links Found: ")
+	for _, link := range links {
+		fmt.Print(link)
+	}
 }
